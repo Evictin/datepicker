9867dd2378f9318d675d26cf369637787f72e41a
Jesse Domack <Jesse Domack>
2017-02-05 20:03:44 -0600

    modified the events for saving date and time

--- app/components/date-picker.js
+++ app/components/date-picker.js
@@ -5,40 +5,57 @@ export default Ember.Component.extend({
     hours: ["HH","01","02","03","04","05","06","07","08","09","10","11","12"],
     minutes: ["MM","00","15","30","45"],
     ampms: ["--","AM","PM"],
-    time: Ember.computed('hour','min','ampm', function() {
-      var time = this.get('hour') + ":" + this.get('minute');
-      console.log(time);
-      return time;
+    partOfDateChanged: Ember.observer('hour','minute','ampm', 'date', function() {
+      Ember.run.once(this, 'calculateNewDate');
     }),
+    calculateNewDate: function() {
+      var minute = this.get('minute');
+      var ampm = this.get('ampm');
+      var hour = this.get('hour') % 12 + (ampm === 'PM' ? 12 : 0);
+      var oldVal = this.get('date');
+      var year = oldVal.getFullYear();
+      var month = oldVal.getMonth();
+      var date = oldVal.getDate();
+      this.set('value', new Date(year, month, date, hour, minute, 0, 0));
+    },
     init: function() {
       this._super(...arguments);
-      console.log("INIT()");
       this.set('hour', format(this.get('value'), 'HH'));
       this.set('minute', format(this.get('value'), 'mm'));
       this.set('ampm', format(this.get('value'), 'A'));
     },
     didInsertElement: function() {
+      var _this = this;
       this.$('input[type=text]').datepicker({
-        showOn:'button'
+        showOn:'button',
+        showButtonPanel:true,
+        onSelect: function(dateText) {
+        },
+        onClose: function(dateText) {
+          var date = new Date(dateText);
+          _this.set('date', date);
+        }
       });
+      // the following code prevents the datepicker from closing when you click on a date
+      $.datepicker._selectDateOverload = $.datepicker._selectDate;
+      $.datepicker._selectDate = function(id, dateStr) {
+          var target = $(id);
+          var inst = this._getInst(target[0]);
+          inst.inline = true;
+          $.datepicker._selectDateOverload(id, dateStr);
+          inst.inline = false;
+          this._updateDatepicker(inst);
+      }
     },
     actions: {
       changeHour(value) {
-        console.log("changeHour()");
         this.set('hour', value);
-        var hour = value % 12 + (this.get('ampm') === 'PM' ? 12 : 0);
-        this.set('value', new Date(this.get('value').setHours(hour)));
       },
       changeMinute(value) {
-        console.log("changeMinute()");
         this.set('minute', value);
-        this.set('value', new Date(this.get('value').setMinutes(value)));
       },
       changeAmpm(value) {
-        console.log("changeAmpm()");
         this.set('ampm', value);
-        var hour = this.get('hour') % 12 + (value === 'PM' ? 12 : 0);
-        this.set('value', new Date(this.get('value').setHours(hour)));
       }
     }
 
